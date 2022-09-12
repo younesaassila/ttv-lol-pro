991a73584badf875a5fe389a8765062413c81b30
Younes Aassila <47226184+younesaassila@users.noreply.github.com>
2022-09-12 14:28:12 +0200

    🍱 Add inactive icon

--- src/background/background.ts
+++ src/background/background.ts
@@ -28,3 +28,25 @@ browser.webRequest.onHeadersReceived.addListener(
   { urls: ["https://api.ttv.lol/playlist/*", "https://api.ttv.lol/vod/*"] },
   ["blocking"]
 );
+
+// Update browser action icon.
+browser.tabs.onActivated.addListener(async () => {
+  const icon = await getBrowserActionIcon();
+  browser.browserAction.setIcon(icon);
+});
+
+async function getBrowserActionIcon(): Promise<browser.Action.SetIconDetailsType> {
+  const ICON_GRAY_PATH = new URL("../assets/icon-gray.png", import.meta.url);
+  const ICON_PATH = new URL("../assets/icon.png", import.meta.url);
+
+  const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+  const activeTab = tabs[0];
+  if (!activeTab || !activeTab.url) return { path: ICON_GRAY_PATH.href };
+  try {
+    const url = new URL(activeTab.url);
+    if (url.hostname === "www.twitch.tv") {
+      return { path: ICON_PATH.href };
+    }
+  } catch {}
+  return { path: ICON_GRAY_PATH.href };
+}
