0a1436c3a6325f9e23534db3eedb2e57db6523e3
Younes Aassila <47226184+younesaassila@users.noreply.github.com>
2022-09-21 14:29:11 +0200

    ✨ Add whitelist button to popup

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -9,6 +9,9 @@ const redirectedElement = $("#redirected") as HTMLSpanElement;
 const streamIdElement = $("#stream-id") as HTMLSpanElement;
 const reasonElement = $("#reason") as HTMLElement;
 const proxyCountryElement = $("#proxy-country") as HTMLElement;
+const whitelistToggleWrapper = $("#whitelist-toggle-wrapper") as HTMLDivElement;
+const whitelistToggle = $("#whitelist-toggle") as HTMLInputElement;
+const whitelistToggleLabel = $("#whitelist-toggle-label") as HTMLLabelElement;
 
 store.addEventListener("load", async () => {
   const tabs = await browser.tabs.query({
@@ -28,8 +31,10 @@ store.addEventListener("load", async () => {
   if (status != null) {
     streamStatusElement.style.display = "flex";
     if (status.redirected) {
+      redirectedElement.classList.remove("error");
       redirectedElement.classList.add("success");
     } else {
+      redirectedElement.classList.remove("success");
       redirectedElement.classList.add("error");
     }
     streamIdElement.textContent = toTitleCase(streamId);
@@ -43,5 +48,32 @@ store.addEventListener("load", async () => {
     } else {
       proxyCountryElement.style.display = "none";
     }
+
+    whitelistToggle.checked =
+      store.state.whitelistedChannels.includes(streamId);
+    whitelistToggle.addEventListener("change", e => {
+      const target = e.target as HTMLInputElement;
+      if (target.checked) {
+        store.state.whitelistedChannels.push(streamId);
+      } else {
+        store.state.whitelistedChannels =
+          store.state.whitelistedChannels.filter(id => id !== streamId);
+      }
+      updateWhitelistToggleLabel(target.checked);
+      browser.tabs.reload();
+    });
+    updateWhitelistToggleLabel(whitelistToggle.checked);
+    whitelistToggleWrapper.style.display = "block";
+  } else {
+    streamStatusElement.style.display = "none";
+    whitelistToggleWrapper.style.display = "none";
   }
 });
+
+function updateWhitelistToggleLabel(checked: boolean) {
+  if (checked) {
+    whitelistToggleLabel.textContent = "✓ Whitelisted";
+  } else {
+    whitelistToggleLabel.textContent = "+ Whitelist";
+  }
+}
