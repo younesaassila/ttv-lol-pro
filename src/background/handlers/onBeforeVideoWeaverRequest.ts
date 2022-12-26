6e8757cab68be49e212646d893f1bbe5ffd3ab66
Younes Aassila <47226184+younesaassila@users.noreply.github.com>
2022-12-26 17:20:25 +0100

    🎨 Improve code

--- src/background/handlers/onBeforeVideoWeaverRequest.ts
+++ src/background/handlers/onBeforeVideoWeaverRequest.ts
@@ -1,81 +1,92 @@
 import browser, { WebRequest } from "webextension-polyfill";
-import { MidrollMessage } from "../../types";
+import type { MidrollMessage } from "../../types";
 
-const hasTabMidroll = new Map<number, boolean>();
+const AD_SIGNIFIER = "stitched"; // From https://github.com/cleanlock/VideoAdBlockForTwitch/blob/145921a822e830da62d39e36e8aafb8ef22c7be6/firefox/content.js#L87
+const URL_REGEX = /^https?:\/\/video-weaver\.(?:[a-z0-9-]+\.)*ttvnw\.net\//i;
+const lastMidrollStartDateString = new Map<number, string>(); // Tab ID -> Start Date String.
 
 export default function onBeforeVideoWeaverRequest(
   details: WebRequest.OnBeforeRequestDetailsType
-) {
-  // TODO: Improve this check (using regex)
-  if (!details.url.toLowerCase().startsWith("https://video-weaver.")) return {};
+): WebRequest.BlockingResponseOrPromise {
+  if (!URL_REGEX.test(details.url)) return {};
+  if (!browser.webRequest.filterResponseData) return {};
 
-  const supportsFilterResponseData = // Chrome doesn't support `filterResponseData`.
-    browser.webRequest.filterResponseData != null;
-  // Detect midrolls by looking for the AD_SIGNIFIER string in the video weaver response.
-  if (supportsFilterResponseData) {
-    const AD_SIGNIFIER = "stitched";
+  const filter = browser.webRequest.filterResponseData(details.requestId);
+  const decoder = new TextDecoder("utf-8");
+  const encoder = new TextEncoder();
 
-    const filter = browser.webRequest.filterResponseData(details.requestId);
-    const decoder = new TextDecoder("utf-8");
-    const encoder = new TextEncoder();
+  const buffers = [] as ArrayBuffer[];
+  filter.ondata = event => buffers.push(event.data);
+  filter.onstop = () => {
+    let responseText = "";
+    for (const [i, buffer] of buffers.entries()) {
+      const stream = i !== buffers.length - 1;
+      responseText += decoder.decode(buffer, { stream });
+    }
+    responseText = filterResponseText(details, responseText);
 
-    const buffers = [] as ArrayBuffer[];
-    filter.ondata = event => buffers.push(event.data);
-    filter.onstop = () => {
-      let responseText = "";
-      for (const [i, buffer] of buffers.entries()) {
-        const stream = i !== buffers.length - 1;
-        responseText += decoder.decode(buffer, { stream });
-      }
-
-      // From https://github.com/cleanlock/VideoAdBlockForTwitch/blob/145921a822e830da62d39e36e8aafb8ef22c7be6/firefox/content.js#L523-L527
-      const hasAdTags = (text: string) => text.includes(AD_SIGNIFIER);
-      const isMidroll = (text: string) =>
-        text.includes('"MIDROLL"') || text.includes('"midroll"');
-      if (hasAdTags(responseText) && isMidroll(responseText)) {
-        // Prevent multiple midroll messages from being sent for the same midroll.
-        if (hasTabMidroll.get(details.tabId) === true) return;
-        hasTabMidroll.set(details.tabId, true);
-
-        const responseTextLines = responseText.split("\n");
-        const midrollLine = responseTextLines.find(
-          line => hasAdTags(line) && isMidroll(line)
-        );
-        if (!midrollLine) return;
-
-        // From https://stackoverflow.com/a/3143231
-        const startDateRegex =
-          /START-DATE="(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+(?:[+-][0-2]\d:[0-5]\d|Z))"/i;
-        const startDateMatch = midrollLine.match(startDateRegex);
-        if (!startDateMatch) return;
-        const [, startDateString] = startDateMatch;
-
-        console.log(
-          `Tab (ID = ${details.tabId}): Detected midroll (startDateString = "${startDateString}")`
-        );
-        console.log(responseText); // TODO: Remove this.
-
-        const message = {
-          type: "midroll",
-          response: {
-            tabId: details.tabId,
-            startDateString,
-          },
-        } as MidrollMessage;
-        browser.tabs.sendMessage(details.tabId, message).catch(console.error);
-      } else {
-        if (hasTabMidroll.get(details.tabId) === true) {
-          console.log(`Tab (ID = ${details.tabId}): No midroll detected`);
-          console.log(responseText); // TODO: Remove this.
-        }
-
-        hasTabMidroll.set(details.tabId, false);
-      }
-
-      filter.write(encoder.encode(responseText));
-      filter.close();
-    };
-  }
+    filter.write(encoder.encode(responseText));
+    filter.close();
+  };
 
   return {};
+}
+
+/**
+ * Detect midrolls by looking for an ad signifier in the video weaver response.
+ * @param details
+ * @param responseText
+ * @returns
+ */
+function filterResponseText(
+  details: WebRequest.OnBeforeRequestDetailsType,
+  responseText: string
+): string {
+  // From https://github.com/cleanlock/VideoAdBlockForTwitch/blob/145921a822e830da62d39e36e8aafb8ef22c7be6/firefox/content.js#L523-L527
+  const hasAdTags = (text: string) => text.includes(AD_SIGNIFIER);
+  const isMidroll = (text: string) =>
+    text.includes('"MIDROLL"') || text.includes('"midroll"');
+
+  const responseTextLines = responseText.split("\n");
+  const midrollLine = responseTextLines.find(
+    line => hasAdTags(line) && isMidroll(line)
+  );
+  if (!midrollLine) return responseText;
+
+  const startDateString = getStartDateStringFromMidrollLine(midrollLine);
+  if (!startDateString) return responseText;
+
+  // Prevent multiple midroll messages from being sent for the same midroll.
+  const lastStartDateString = lastMidrollStartDateString.get(details.tabId);
+  const isSameMidroll = startDateString === lastStartDateString;
+  if (!isSameMidroll) {
+    console.log(
+      `Tab (ID = ${details.tabId}): Detected midroll (Scheduled for ${startDateString})`
+    );
+
+    const message = {
+      type: "midroll",
+      response: {
+        tabId: details.tabId,
+        startDateString,
+      },
+    } as MidrollMessage;
+    browser.tabs.sendMessage(details.tabId, message).catch(console.error);
+
+    lastMidrollStartDateString.set(details.tabId, startDateString);
+  }
+
+  return responseText;
+}
+
+function getStartDateStringFromMidrollLine(
+  midrollLine: string
+): string | undefined {
+  // From https://stackoverflow.com/a/3143231
+  const startDateRegex =
+    /START-DATE="(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+(?:[+-][0-2]\d:[0-5]\d|Z))"/i;
+  const startDateMatch = midrollLine.match(startDateRegex);
+  if (!startDateMatch) return;
+  const [, startDateString] = startDateMatch;
+  return startDateString;
 }
--- src/content/content.ts
+++ src/content/content.ts
@@ -4,16 +4,20 @@ import { TWITCH_URL_REGEX } from "../common/ts/regexes";
 import store from "../store";
 import type { CurrentTabIdMessage, Message, MidrollMessage } from "../types";
 
+log("Content script running.");
+
 let currentTabId: number | undefined = undefined;
+
+// Query current tab ID.
+const message = {
+  type: "currentTabId",
+} as CurrentTabIdMessage;
+browser.runtime.sendMessage(message).catch(console.error);
 
 if (store.readyState === "complete") main();
 else store.addEventListener("load", main);
 
-console.log("[TTV LOL PRO] Content script loaded.");
-
-async function main() {
-  console.log("[TTV LOL PRO] Content script running.");
-
+function main() {
   const match = TWITCH_URL_REGEX.exec(location.href);
   if (!match) return;
   const [, streamId] = match;
@@ -23,41 +27,45 @@ async function main() {
     // Clear errors for stream on page load/reload.
     store.state.streamStatuses[streamId].errors = [];
   }
-
-  const message = {
-    type: "currentTabId",
-  } as CurrentTabIdMessage;
-  browser.runtime.sendMessage(message).catch(console.error);
 }
 
-browser.runtime.onMessage.addListener((message: Message, sender) => {
-  console.log("[Content Script] Message received:", message);
+browser.runtime.onMessage.addListener(onMessage);
 
+function onMessage(message: Message, sender: browser.Runtime.MessageSender) {
   if (sender.id !== browser.runtime.id) return;
 
-  switch (message.type) {
-    case "currentTabId":
-      currentTabId = (message as CurrentTabIdMessage).response.tabId;
-      break;
-    case "midroll":
-      const { tabId, startDateString } = (message as MidrollMessage).response;
-      if (tabId !== currentTabId) break;
-      const startDate = new Date(startDateString);
-      const now = new Date();
-      setTimeout(() => {
-        // Check if FrankerFaceZ's reset player button exists.
-        const ffzResetPlayerButton = $(
-          'button[data-a-target="ffz-player-reset-button"]'
+  if (message.type === "currentTabId") {
+    // Store current tab ID.
+    const { tabId } = (message as CurrentTabIdMessage).response;
+    currentTabId = tabId;
+  } else if (message.type === "midroll") {
+    // Reset player if midroll detected.
+    const { tabId, startDateString } = (message as MidrollMessage).response;
+    if (currentTabId == null || tabId !== currentTabId) return;
+
+    const now = new Date();
+    const startDate = new Date(startDateString);
+    const delay = now.getTime() - startDate.getTime();
+    log(`Midroll scheduled for ${startDateString} (in ${delay} ms)`);
+    if (delay < 0) return;
+
+    setTimeout(() => {
+      // Check if FrankerFaceZ's reset player button exists.
+      const ffzResetPlayerButton = $(
+        'button[data-a-target="ffz-player-reset-button"]'
+      );
+      if (ffzResetPlayerButton) {
+        ffzResetPlayerButton.dispatchEvent(
+          new MouseEvent("dblclick", { bubbles: true })
         );
-        if (ffzResetPlayerButton) {
-          ffzResetPlayerButton.dispatchEvent(
-            new MouseEvent("dblclick", { bubbles: true })
-          );
-          console.log("Clicked FFZ reset player button.");
-        } else {
-          // TODO: Notify injected script to reset player or reload page.
-        }
-      }, now.getTime() - startDate.getTime()); // TODO: Check if this is precise enough.
-      break;
+        log("Clicked FrankerFaceZ's reset player button.");
+      } else {
+        // TODO: Notify injected script to reset player or reload page.
+      }
+    }, delay);
   }
-});
+}
+
+function log(...args: any[]) {
+  console.log("[TTV LOL PRO]", ...args);
+}
