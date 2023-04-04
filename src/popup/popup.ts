d721518e014a6d5945ac168e3e1c535320d39ae5
Younes Aassila <47226184+younesaassila@users.noreply.github.com>
2023-04-04 19:02:52 +0200

    🚧 Done remake of Whitelist button

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -2,6 +2,7 @@ import browser from "webextension-polyfill";
 import $ from "../common/ts/$";
 import { TWITCH_URL_REGEX } from "../common/ts/regexes";
 import store from "../store";
+import type { StreamStatus } from "../types";
 
 //#region HTML Elements
 const updateBannerElement = $("#update-banner") as HTMLDivElement;
@@ -11,8 +12,7 @@ const streamIdElement = $("#stream-id") as HTMLSpanElement;
 const reasonElement = $("#reason") as HTMLElement;
 const proxyCountryElement = $("#proxy-country") as HTMLElement;
 const whitelistStatusElement = $("#whitelist-status") as HTMLDivElement;
-const whitelistToggle = $("#whitelist-toggle") as HTMLInputElement;
-const whitelistToggleLabel = $("#whitelist-toggle-label") as HTMLLabelElement;
+const whitelistToggleElement = $("#whitelist-toggle") as HTMLInputElement;
 //#endregion
 
 if (store.readyState === "complete") main();
@@ -41,59 +41,59 @@ function setStreamStatusElement(streamId: string) {
   const streamIdLower = streamId.toLowerCase();
   const status = store.state.streamStatuses[streamIdLower];
   if (status) {
+    setProxyStatus(streamIdLower, status);
+    setWhitelistStatus(streamIdLower);
     streamStatusElement.style.display = "flex";
-    if (status.redirected) {
-      redirectedElement.classList.remove("error");
-      redirectedElement.classList.add("success");
-    } else {
-      redirectedElement.classList.remove("success");
-      redirectedElement.classList.add("error");
-    }
-    streamIdElement.textContent = streamId;
-    if (status.reason) {
-      reasonElement.textContent = status.reason;
-    } else {
-      reasonElement.style.display = "none";
-    }
-    if (status.proxyCountry) {
-      proxyCountryElement.textContent = `Proxy country: ${status.proxyCountry}`;
-    } else {
-      proxyCountryElement.style.display = "none";
-    }
   } else {
     streamStatusElement.style.display = "none";
   }
-  setWhitelistToggleElement(streamId);
 }
 
-function setWhitelistToggleElement(streamId: string) {
-  const streamIdLower = streamId.toLowerCase();
-  const status = store.state.streamStatuses[streamIdLower];
-  if (status) {
-    whitelistToggle.checked =
-      store.state.whitelistedChannels.includes(streamId);
-    whitelistToggle.addEventListener("change", e => {
-      const target = e.target as HTMLInputElement;
-      if (target.checked) {
-        store.state.whitelistedChannels.push(streamId);
-      } else {
-        store.state.whitelistedChannels =
-          store.state.whitelistedChannels.filter(id => id !== streamId);
-      }
-      updateWhitelistToggleLabel(target.checked);
-      browser.tabs.reload();
-    });
-    updateWhitelistToggleLabel(whitelistToggle.checked);
-    whitelistStatusElement.style.display = "flex";
+function setProxyStatus(streamIdLower: string, status: StreamStatus) {
+  // Redirected
+  if (status.redirected) {
+    redirectedElement.classList.remove("error");
+    redirectedElement.classList.add("success");
   } else {
-    whitelistStatusElement.style.display = "none";
+    redirectedElement.classList.remove("success");
+    redirectedElement.classList.add("error");
+  }
+  // Stream ID
+  streamIdElement.textContent = streamIdLower;
+  // Reason
+  if (status.reason) {
+    reasonElement.textContent = status.reason;
+  } else {
+    reasonElement.style.display = "none";
+  }
+  // Proxy country
+  if (status.proxyCountry) {
+    proxyCountryElement.textContent = `Proxy country: ${status.proxyCountry}`;
+  } else {
+    proxyCountryElement.style.display = "none";
   }
 }
 
-function updateWhitelistToggleLabel(checked: boolean) {
-  if (checked) {
-    whitelistToggleLabel.textContent = "✓ Whitelisted";
-  } else {
-    whitelistToggleLabel.textContent = "+ Whitelist";
-  }
+function setWhitelistStatus(streamIdLower: string) {
+  const whitelistedChannelsLower = store.state.whitelistedChannels.map(id =>
+    id.toLowerCase()
+  );
+  const isWhitelisted = whitelistedChannelsLower.includes(streamIdLower);
+  whitelistStatusElement.setAttribute("data-whitelisted", `${isWhitelisted}`);
+  whitelistToggleElement.checked = isWhitelisted;
+  whitelistToggleElement.addEventListener("change", e => {
+    const target = e.target as HTMLInputElement;
+    const isWhitelisted = target.checked;
+    if (isWhitelisted) {
+      // Add stream ID to whitelist.
+      store.state.whitelistedChannels.push(streamIdLower);
+    } else {
+      // Remove stream ID from whitelist.
+      store.state.whitelistedChannels = store.state.whitelistedChannels.filter(
+        id => id !== streamIdLower
+      );
+    }
+    whitelistStatusElement.setAttribute("data-whitelisted", `${isWhitelisted}`);
+    browser.tabs.reload();
+  });
 }
