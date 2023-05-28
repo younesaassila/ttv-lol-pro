2a03a4a142f639504cb02fbb102c758001704066
Younes Aassila <47226184+younesaassila@users.noreply.github.com>
2023-05-28 13:34:20 +0200

    🐛 Fix Firefox Nightly import bug

--- src/page/page.ts
+++ src/page/page.ts
@@ -4,17 +4,32 @@ console.info("[TTV LOL PRO] 🚀 Page script running.");
 
 const params = JSON.parse(document.currentScript.dataset.params);
 
-self.fetch = fetch;
+window.fetch = fetch;
 
-self.Worker = class Worker extends self.Worker {
+window.Worker = class Worker extends window.Worker {
   constructor(scriptURL: string | URL, options?: WorkerOptions) {
+    const url = scriptURL.toString();
+    let script = "";
+    // Firefox Nightly errors out when trying to import a blob URL directly.
+    if (url.startsWith("blob:")) {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url, false);
+      xhr.send();
+      if (!(200 <= xhr.status && xhr.status < 300)) {
+        throw new Error(`Failed to fetch script: ${xhr.statusText}`);
+      }
+      script = xhr.responseText;
+    }
+    if (!script) {
+      script = `importScripts("${scriptURL}");`;
+    }
     const newScript = `
       try {
         importScripts("${params.workerScriptURL}");
       } catch {
-        console.error("[TTV LOL PRO] ❌ Failed to load worker script.");
+        console.error(\`[TTV LOL PRO] ❌ Failed to load worker script: ${params.workerScriptURL}\`);
       }
-      importScripts("${scriptURL}");
+      ${script}
     `;
     const newScriptURL = URL.createObjectURL(
       new Blob([newScript], { type: "text/javascript" })
