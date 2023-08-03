0234827d0b2577bceb8bcf67c44361890865a357
younesaassila <47226184+younesaassila@users.noreply.github.com>
2023-08-03 17:00:45 +0200

    🐛 Fix bug when tab.id == 0

--- src/background/handlers/onTabCreated.ts
+++ src/background/handlers/onTabCreated.ts
@@ -6,7 +6,7 @@ import { twitchTvHostRegex } from "../../common/ts/regexes";
 import store from "../../store";
 
 export default function onTabCreated(tab: Tabs.Tab): void {
-  if (!tab.url || !tab.id) return;
+  if (!tab.url || tab.id == null) return;
   const host = getHostFromUrl(tab.url);
   if (host != null && twitchTvHostRegex.test(host)) {
     console.log(`➕ Opened Twitch tab: ${tab.id}`);
