9a16e1a2f57a6e39bd8c00a09b52b9d3a1a936bd
younesaassila <47226184+younesaassila@users.noreply.github.com>
2023-10-05 10:49:21 +0200

    🐛 Fix checkForOpenedTwitchTabs bug - Didn't take into account whitelisted tabs

--- src/background/handlers/checkForOpenedTwitchTabs.ts
+++ src/background/handlers/checkForOpenedTwitchTabs.ts
@@ -1,4 +1,6 @@
 import browser from "webextension-polyfill";
+import findChannelFromTwitchTvUrl from "../../common/ts/findChannelFromTwitchTvUrl";
+import isChannelWhitelisted from "../../common/ts/isChannelWhitelisted";
 import isChromium from "../../common/ts/isChromium";
 import {
   clearProxySettings,
@@ -13,18 +15,27 @@ export default function checkForOpenedTwitchTabs() {
   browser.tabs
     .query({ url: ["https://www.twitch.tv/*", "https://m.twitch.tv/*"] })
     .then(tabs => {
-      if (tabs.length === 0) {
-        if (isChromium) clearProxySettings();
-        return;
-      }
       console.log(
         `🔍 Found ${tabs.length} opened Twitch tabs: ${tabs
           .map(tab => tab.id)
           .join(", ")}`
       );
-      if (isChromium) {
-        updateProxySettings();
-      }
       store.state.openedTwitchTabs = tabs;
+
+      if (isChromium) {
+        const allTabsAreWhitelisted = tabs.every(tab => {
+          if (!tab.url) return false;
+          const channelName = findChannelFromTwitchTvUrl(tab.url);
+          const isWhitelisted = channelName
+            ? isChannelWhitelisted(channelName)
+            : false;
+          return isWhitelisted;
+        });
+        if (tabs.length === 0 || allTabsAreWhitelisted) {
+          clearProxySettings();
+        } else {
+          updateProxySettings();
+        }
+      }
     });
 }
