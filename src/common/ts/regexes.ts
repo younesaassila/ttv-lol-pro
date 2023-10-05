4ffa3504c58ccf0a4d1997483613b221d54f2310
younesaassila <47226184+younesaassila@users.noreply.github.com>
2023-10-05 11:47:07 +0200

    🐛 Fix popout chat for whitelisted channels

--- src/common/ts/regexes.ts
+++ src/common/ts/regexes.ts
@@ -1,7 +1,7 @@
 export const passportHostRegex = /^passport\.twitch\.tv$/i;
 export const twitchApiChannelNameRegex = /\/hls\/(.+)\.m3u8/i;
 export const twitchChannelNameRegex =
-  /^https?:\/\/(?:www|m)\.twitch\.tv\/(?:videos\/)?(\w+)/i;
+  /^https?:\/\/(?:www|m)\.twitch\.tv\/(?:videos\/|popout\/)?(\w+)/i;
 export const twitchGqlHostRegex = /^gql\.twitch\.tv$/i;
 export const twitchTvHostRegex = /^(?:www|m)\.twitch\.tv$/i;
 export const usherHostRegex = /^usher\.ttvnw\.net$/i;
